66d5a42fdc6aaf591477e09cde079dbfebf27187
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-08-23 20:16:09 +0000

    Initial commit

---
+++ Calculation.java
@@ -0,0 +1,12 @@
+public class Calculation {
+
+	public static void addition(double number1, double number2) {
+		double res = number1 + number2;
+		System.out.println("Addition is "+res);
+	}
+	
+	public static void substraction(double number1, double number2) {
+		double res = number1 - number2;
+		System.out.println("Substraction is "+res);
+	}
+}
---
+++ MainDemoApp.java
@@ -0,0 +1,10 @@
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		Calculation.addition(34.5, 56);
+		Calculation.substraction(23, 4.6);
+	}
+
+}
